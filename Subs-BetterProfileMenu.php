5cd15bfce600de73fd9173f42267844afdc6955e
douglasorend <37510276+douglasorend@users.noreply.github.com>
2018-10-11 10:35:48 -0500

    v1.2 - August 2nd, 2014
    v1.2 - August 2nd, 2014
    o Rewrote mod so that menu replacement takes place in Subs-BetterProfileMenu.php....
    o Added in code to preserve the original language text during the hook call...
    
    v1.1 - August 1st, 2014
    o Fixed errors generated by not having the Profiles language file loaded.

---
+++ Subs-BetterProfileMenu.php
@@ -0,0 +1,76 @@
+<?php
+/**********************************************************************************
+* Subs-BetterProfile.php - Subs of the Lazy Admin Menu mod
+*********************************************************************************
+* This program is distributed in the hope that it is and will be useful, but
+* WITHOUT ANY WARRANTIES; without even any implied warranty of MERCHANTABILITY
+* or FITNESS FOR A PARTICULAR PURPOSE, 
+**********************************************************************************/
+if (!defined('SMF')) 
+	die('Hacking attempt...');
+	
+/**********************************************************************************
+* Better Profile Menu hook
+**********************************************************************************/
+function BetterProfile_Menu_Buttons(&$areas)
+{
+	global $txt, $scripturl, $context;
+	
+	// DO NOT RUN if $_GET['xml'] is defined!
+	if (isset($_GET['xml']))
+		return;
+		
+	// Load the Profile language, preserving the "time_format" string:
+	$old_txt = $txt;
+	loadLanguage('Profile');
+	
+	// Redefine the Profiles menu:
+	$areas['profile'] = array(
+		'title' => $txt['profile'],
+		'href' => $scripturl . '?action=profile',
+		'show' => $context['allow_edit_profile'],
+		'sub_buttons' => array(
+			'summary' => array(
+				'title' => $txt['summary'],
+				'href' => $scripturl . '?action=profile',
+				'show' => true,
+			),
+			'account' => array(
+				'title' => $txt['account'],
+				'href' => $scripturl . '?action=profile;area=account',
+				'show' => allowedTo(array('profile_identity_any', 'profile_identity_own', 'manage_membergroups')),
+			),
+			'profile' => array(
+				'title' => $txt['forumprofile'],
+				'href' => $scripturl . '?action=profile;area=forumprofile',
+				'show' => allowedTo(array('profile_extra_any', 'profile_extra_own')),
+			),
+			'posts' => array(
+				'title' => $txt['showPosts'],
+				'href' => $scripturl . '?action=profile;area=showposts',
+				'show' => allowedTo(array('profile_extra_any', 'profile_extra_own')),
+			),
+			'notification' => array(
+				'title' => $txt['notification'],
+				'href' => $scripturl . '?action=profile;area=notification',
+				'show' => allowedTo(array('profile_extra_any', 'profile_extra_own')),
+			),
+			'ignore' => array(
+				'title' => $txt['editBuddyIgnoreLists'],
+				'href' => $scripturl . '?action=profile;area=lists;sa=ignore',
+				'show' => allowedTo(array('profile_extra_any', 'profile_extra_own')),
+				'is_last' => true,
+			),
+			'theme' => array(
+				'title' => $txt['theme'],
+				'href' => $scripturl . '?action=profile;area=theme',
+				'show' => allowedTo(array('profile_extra_any', 'profile_extra_own')),
+			),
+		),
+	);
+	
+	// Restore the language strings:
+	$txt = $old_txt;
+}
+
+?>
